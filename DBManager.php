8f2ebdb5872a44f5b04312755cc0f13364492bc1
Jahid Hasan Alvi <jahidhasanalvi007@gmail.com>
2021-01-13 05:01:33 +0600

    Create DBManager.php

---
+++ DBManager.php
@@ -0,0 +1,52 @@
+<?php
+function conectionStart()
+{/*
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "testDB";
+$connection = mysqli_connect($servername, $username, $password,
+$dbname);*/
+ $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "findnest";
+  $connection = mysqli_connect($servername, $username, $password, $dbname);
+
+return $connection;
+}
+function conectionEnd($value)
+{
+mysqli_close($value);
+}
+function fetch($value='')
+{
+$connection = conectionStart();
+$sql = "SELECT * FROM Contact WHERE name LIKE '%".$value."%' OR
+email LIKE '%".$value."%' OR comments LIKE '%".$value."%'";
+$result = mysqli_query($connection, $sql);
+if($result) {
+
+echo "<table>";
+echo "<tr>";
+echo "<th>Name</th>";
+echo "<th>Email</th>";
+echo "<th>Comments</th>";
+echo "</tr>";
+while ($row = mysqlI_fetch_assoc($result)) {
+echo "<tr>";
+echo "<td>".$row['name']."</td>";
+echo "<td>".$row['email']." </td>";
+echo "<td>".$row['comments']."</td>";
+echo "</tr>";
+}
+echo "</table>";
+} else {
+echo "Error :".$sql."<br>".mysqli_error($connection);
+}
+conectionEnd($connection);
+}
+if (isset($_GET['search'])) {
+fetch($_GET['search']);
+}
+?>
