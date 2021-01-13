08cac448711f02bcebda5909e986be33ac838c0c
Jahid Hasan Alvi <jahidhasanalvi007@gmail.com>
2021-01-14 05:14:25 +0600

    Update Registration_vendor.php

--- Registration_vendor.php
+++ Registration_vendor.php
@@ -30,8 +30,8 @@ session_destroy();
 
     <body>
         <header>
-              <?php include 'header.php';?>
-            <h1>Find Nest | Vendor Registration</h1> </header>
+            <?php include 'header.php';?>
+                <h1>Find Nest | Vendor Registration</h1> </header>
         <div>
             <form name="LoginForms" class="" action="Registration_vendor.php" method="POST" onsubmit="return validateForm();">
                 <label>Name:</label>
@@ -111,8 +111,7 @@ if($fname != ""){
 mysqli_close($conn);
 }
 ?>
-
-      <?php include 'footer.php'; ?>  
+            <?php include 'footer.php'; ?>
     </body>
     <script type="text/javascript">
         function validateForm() {
