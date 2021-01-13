fa313beffe425e454c3126d8e3572866fd059590
Jahid Hasan Alvi <jahidhasanalvi007@gmail.com>
2021-01-14 05:13:55 +0600

    Update footer.php

--- footer.php
+++ footer.php
@@ -2,7 +2,9 @@
 <html>
 <head>
 	<title></title>
-    
+	<style type="text/css">
+		
+	</style>
 </head>
 <body>
 <footer style="text-align: center;">&copy 2021 FindNest. All Rights Reserved</footer>
