46c90c9b5dd8cfd4fbb6e0820d041c2d049ea9a5
Jahid Hasan Alvi <jahidhasanalvi007@gmail.com>
2021-01-14 05:14:05 +0600

    Update Dashboard.php

--- Dashboard.php
+++ Dashboard.php
@@ -15,12 +15,12 @@ if(empty($_SESSION["username"]))
 }
 */
 ?>
+    <!DOCTYPE html>
+    <html lang="en" dir="ltr">
 
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-<head>
-	<title>Dashboard | Find Nest</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
+    <head>
+        <title>Dashboard | Find Nest</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
         <style type="text/css">
             body {
                 background-color: snow;
@@ -29,66 +29,54 @@ if(empty($_SESSION["username"]))
             input {
                 left: 100px;
             }
+            
             table {
                 width: 100%;
             }
-        </style>    
+        </style>
+        <script>
+            function usersearchTxt(str) {
+                console.log(str);
+            }
+        </script>
+    </head>
 
-	<script>
-function usersearchTxt(str) {
-console.log(str);
-}
-</script>
-
-	</head>
-<body>
-
-	<header>
-      <?php include 'header.php';?>
-        <h1>Find Nest | Dashboard</h1>
-	</header>
-
-
-	<div>
-		<h1>You are Logged In</h1>
-	</div>
-	<div>
-	<input id="searchBox" type="text" name="search" value="" placeholder="Enter key to search" onkeyup="usersearchTxt(this.value)">
-<!--<button type="button" name="button"
+    <body>
+        <header>
+            <?php include 'header.php';?>
+                <h1>Find Nest | Dashboard</h1> </header>
+        <div>
+            <h1>You are Logged In</h1> </div>
+        <div>
+            <input id="searchBox" type="text" name="search" value="" placeholder="Enter key to search" onkeyup="usersearchTxt(this.value)">
+            <!--<button type="button" name="button"
 onclick="usersearchTxt(document.getElementById('searchBox').value);">search</button> -->
-<div id="searchTable">
-<table width="100%">
-	  <tr>
-	  	<th  width= "30%">Name</th>
-	  	<th  width= "30%">Email</th>
-	  	<th  width= "40%">Comments</th>
-	  </tr>
-
-<?php
+            <div id="searchTable">
+                <table width="100%">
+                    <tr>
+                        <th width="30%">Name</th>
+                        <th width="30%">Email</th>
+                        <th width="40%">Comments</th>
+                    </tr>
+                    <?php
 include 'DBManager.php';
 echo fetch('');
 ?>
-</table>
-    </div>
-<a href="Logout.php">Logout</a>
-        
-    </div>
-	<footer style="text-align: center;">&copy 2021 Find Nest. All Rights Reserved</footer>
-</body>
+                </table>
+            </div> <a href="Logout.php">Logout</a> </div>
+        <footer style="text-align: center;">&copy 2021 Find Nest. All Rights Reserved</footer>
+    </body>
+    <script type="text/javascript">
+        function usersearchTxt(str) {
+            var xmlhttp = new XMLHttpRequest();
+            xmlhttp.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    document.getElementById('searchTable').innerHTML = this.responseText;
+                }
+            }
+            xmlhttp.open("GET", "DBManager.php?search=" + str, true);
+            xmlhttp.send();
+        }
+    </script>
 
-  <script type="text/javascript">
-
-function usersearchTxt(str) {
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function () {
-if(this.readyState == 4 && this.status == 200){
-document.getElementById('searchTable').innerHTML =
-this.responseText;
-}
-}
-xmlhttp.open("GET","DBManager.php?search="+str,true);
-xmlhttp.send();
-}
-</script>
-
-</html>
+    </html>
