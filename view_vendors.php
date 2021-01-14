0dfc96d2793ecac0f40ea42f7dd2f9502c7bc7f7
Jahid Hasan Alvi <jahidhasanalvi007@gmail.com>
2021-01-14 09:49:18 +0600

    connection fixed

--- view_vendors.php
+++ view_vendors.php
@@ -1,14 +1,5 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "findnest";
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-
-
-if (!$conn) {
-die("Connection failed: " . mysqli_connect_error());
-}
+include 'connection.php';
 /*
 session_start();
 if(empty($_SESSION["username"]))
