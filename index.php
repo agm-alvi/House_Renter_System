356fe1ec5a68acd32b7d5d3fc3de03d64d19b66c
Jahid Hasan Alvi <jahidhasanalvi007@gmail.com>
2021-01-16 19:46:39 +0600

    last uploaded code

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>House Rental System</title>
+    <link rel="stylesheet" type="text/css" href="homepagestyle.css"> </head>
+
+<body>
+    <header>
+        <div class="main">
+            <div class="logo"> <img src="h.jpg"> </div>
+            <ul>
+                <li class="index.php"><a href="#"> Home </a></li>
+                <li> <a href="service.php"> Service </a> </li>
+                <li> <a href="Contact.php"> About Us </a> </li>
+                <li> <a href="#"> Contact Us </a> </li>
+                <li> <a href="#"> Our Reviews </a> </li>
+            </ul>
+        </div>
+        <div class="welcome">
+            <h1> Welcome to Find Nest</h1> </div>
+        <div class="button">
+            <ol><li><div class="dropdown">
+                <button class="dropbtn">Log In</button>
+                <div class="dropdown-content"> <a href="login_renter.php">Renter Log In</a> <a href="login_vendor.php">Vendor Log In</a> </div>
+            </div></li>
+                <li><div class="dropdown">
+                <button class="dropbtn">Sign Up</button>
+                <div class="dropdown-content"> <a href="Registration_renter.php">Renter Sign Up</a> <a href="Registration_vendor.php">Vendor Sign Up</a> </div>
+            </div></li>
+        </ol></div>
+    </header>
+</body>
+
+</html>
---
+++ service.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Services</title>
+    <link rel= "stylesheet" type="text/css" href= "service.css">
+    <link rel= "stylesheet" type="text/css" href= "homepagestyle.css">
+</head>
+<body>
+   
+    <ul>
+        <li class="home"><a href = "#"> Home </a> <li>
+        <li> <a href = "service.php"> Service </a> <li>
+        <li> <a href = "#"> About Us </a> <li>
+        <li> <a href = "#"> Contact Us </a> <li>
+        <li> <a href = "#"> Our Reviews </a> <li>
+    </ul>
+
+    <div class="servicesheader">
+
+        <h1 class="section-title">Our Services</h1>
+
+            <div class="service">
+            <div class="service-icon"> </div>
+            <div class="service-title"> House Listing </div>
+            <div class="description">
+                You can list your house in our program and we will find someone rent it to..
+            </div>
+            </div>
+
+            <div class="service">
+            <div class="service-icon"> </div>
+            <div class="service-title"> Vendoring Facility </div>
+            <div class="description">
+                We offer vendoring Facility for larger scale rentings.
+            </div>
+            </div>
+
+            <div class="service">
+            <div class="service-icon"> </div>
+            <div class="service-title"> Rent Bookings </div>
+            <div class="description">
+                Book a house any time and any date you feel like, its that simple.
+            </div>
+            </div>
+
+
+            </div>
+    
+    
+</body>
+</html>
