3787e2bd49010a85c32756d3cc306823f02b1665
Jahid Hasan Alvi <jahidhasanalvi007@gmail.com>
2021-01-16 19:47:15 +0600

    Update footer.php

--- footer.php
+++ footer.php
@@ -3,7 +3,14 @@
 <head>
 	<title></title>
 	<style type="text/css">
-		
+		footer {
+    font-weight: bold;
+    border: 2px dotted red;
+    box-shadow: 2px 2px 5px 5px red;
+    margin-top: 15px;
+            margin-bottom: 5px;
+}
+
 	</style>
 </head>
 <body>
