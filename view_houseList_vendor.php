463630ba50d819b7d35c8a313494e1bcaa2ef697
Jahid Hasan Alvi <jahidhasanalvi007@gmail.com>
2021-01-19 19:36:19 +0600

    Create view_houseList_vendor.php

---
+++ view_houseList_vendor.php
@@ -0,0 +1,110 @@
+<?php
+include 'connection.php';
+session_start();
+if(empty($_SESSION["username"]))
+{
+  header('Location: login_renter.php');
+}else{
+  if($_SESSION["username"] == "admin")
+  {
+    header('Location: view_bookings.php');
+  }
+}
+$username = $_SESSION["username"];
+
+$retrieve = "SELECT * FROM vendors v WHERE v.username = '".$username."'";
+$retrieve = mysqli_query($conn, $retrieve);
+$ret = mysqli_fetch_array($retrieve);
+$vid  =  $ret['vendorID'];
+$fname = $ret['fullname'];
+
+$result = "SELECT f.*, h.* FROM favourites f  JOIN houselist h ON f.hID = h.houseID WHERE f.vID = '".$vid."'";
+$result = mysqli_query($conn, $result);
+
+$msg="";
+if(isset($_POST['submit']))
+{
+  
+}
+?>
+    <!DOCTYPE html>
+    <html>
+
+    <head>
+        <title>
+            <?php echo $username?> | Favourite List | Find Nest</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <style type="text/css">
+            body {
+                background-color: navajowhite;
+            }
+            
+            table {
+                width: 100%;
+                left: 10px;
+                text-align: center;
+            }
+        </style>
+    </head>
+
+    <body>
+        <?php include 'header.php';?>
+            <?php
+          if(isset($_POST['submit']))
+          { 
+          ?>
+                <div> <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                    <?php echo $msg;?>
+                </div>
+                <?php
+          }
+      ?>
+                    <h1>Find Nest | Favourite List</h1>
+                    <h1>Renter: <?php echo $fname?> </h1>
+                    <div>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>House ID</th>
+                                    <th>House Type</th>
+                                    <th>Address</th>
+                                    <th>Location</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <?php 
+                    while($res = mysqli_fetch_array($result)) { 
+                        if($res['fID']!=0) {
+                    ?>
+                                <tbody>
+                                    <tr>
+                                        <td>
+                                            <?php echo $res['fID']; ?>
+                                        </td>
+                                        <td>
+                                            <?php echo $res['houseType']; ?>
+                                        </td>
+                                        <td>
+                                            <?php echo $res['house_no']." ".$res['street_no']." ".$res['area']; 
+                                    ?>
+                                        </td>
+                                        <td>
+                                            <?php echo $res['location'];?>
+                                        </td>
+                                        <td>
+                                            <form action="view_houseList_vendor.php" method="post">
+                                                <input type="hidden" name="contact" value="<?php echo $res['houseID'] ?>">
+                                                <input type="submit" name="submit" value="Contact Renter"> </form>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                                <?php   
+                    }
+                }
+                    ?>
+                        </table>
+                    </div>
+                    <?php include 'footer.php'; ?>
+    </body>
+
+    </html>
