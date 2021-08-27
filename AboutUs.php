58f5a611a6d3f78bc83783fb88c85813cc8020e0
Jahid Hasan Alvi <jahidhasanalvi007@gmail.com>
2021-08-27 17:19:12 +0600

    Add files via upload

---
+++ AboutUs.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>About Us | Find Nest</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <style type="text/css">
+            body{
+                background-color: linear-gradient(rgba(255, 100, 100, 0.5),rgba(00,00,00,0.8),rgba(00,255,00,0.5)); 
+            }
+            h1 {
+                text-align: center;
+            }
+            a {
+                color: red;
+            }
+            .r1{
+            	width: 5%;
+            }
+            .rp{
+            	width: 30%;
+            }
+            .rs{
+            	width: 5%;
+            }
+            table{
+            	width: 100%;
+            }
+            table, tr, td{
+            	border: 0px;
+            }
+        </style>
+    </head>
+
+    <body>
+        <?php include 'header.php';?>
+            <h1>Find Nest | About Us</h1>
+            <table>
+            	
+            	<tr>
+            		<td class="r1"></td>
+            		<td class="rp"><img src="profile-pic-male.jpg"></td>
+            		<td class="rp"><img src="profile-pic-female.jpg"></td>
+            		<td class="rp"><img src="profile-pic-male.jpg"></td>
+            		<td class="r1"></td>
+            	</tr>
+            	<tr>
+            		<td class="r1"></td>
+            		<td class="rp"></td>
+            		<td class="rp"></td>
+            		<td class="rp"></td>
+            		<td class="r1"></td>
+            	</tr>
+
+            </table>
+           
+                <?php include 'footer.php';?>
+    </body>
+    </html>
